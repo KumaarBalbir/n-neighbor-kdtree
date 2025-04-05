de928e85ed34bef2661ddb00a62b406b62192550
balbir <prasadbalbir1056@gmail.com>
2025-04-05 15:15:18 +0530

    enhance: more verbose error

--- utils/utils.c
+++ utils/utils.c
@@ -1,11 +1,13 @@
 #include "utils.h"
+#include <errno.h>
 
 int load_points(const char *filename, Point points[])
 {
+  printf("Attempting to open file: %s\n", filename);
   FILE *file = fopen(filename, "r");
   if (!file)
   {
-    perror("Unable to open file");
+    printf("Error opening file: %s (errno: %d)\n", strerror(errno), errno);
     return -1;
   }
 
@@ -13,9 +15,13 @@ int load_points(const char *filename, Point points[])
   while (fscanf(file, "%[^,],%lf,%lf\n", points[count].name,
                 &points[count].coords[0], &points[count].coords[1]) == 3)
   {
+    printf("Loaded point %d: %s (%lf, %lf)\n",
+           count, points[count].name,
+           points[count].coords[0], points[count].coords[1]);
     count++;
   }
 
   fclose(file);
+  printf("Total points loaded: %d\n", count);
   return count;
 }
