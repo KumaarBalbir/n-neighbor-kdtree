d825c0a1295ece557bfa1e75b5606d82454fc7e0
balbir <prasadbalbir1056@gmail.com>
2025-04-06 11:41:24 +0530

    refactor: to accept args from py subprocess

--- src/main.c
+++ src/main.c
@@ -7,9 +7,15 @@
 #include "point.h"
 #include "../utils/utils.h"
 
-int main()
-{
+int main(int argc, char *argv[]) {
 
+    if (argc != 3) {
+        printf("{\"error\": \"Usage: ./nearest_neighbor <lat> <lon>\"}\n");
+        return 1;
+    }
+    double user_lat = atof(argv[1]);
+    double user_lon = atof(argv[2]);
+  // load points from csv
   Point points[MAX_POINTS];
   int n = load_points("data/zomato_locations.csv", points, MAX_POINTS);
 
@@ -19,22 +25,31 @@ int main()
     return 1;
   }
 
+  // build kd tree
   KDNode *root = build_kd_tree(points, 0, n - 1, 0);
 
   Point user_location;
-  printf("Enter your location (lat lon): ");
-  scanf("%lf %lf", &user_location.coords[0], &user_location.coords[1]);
+  // printf("Enter your location (lat lon): ");
+  // if (scanf("%lf %lf", &user_location.coords[0], &user_location.coords[1]) != 2) {
+  //   fprintf(stderr, "Invalid input. Please enter two numbers (latitude longitude).\n");
+  //   return 1; // Exit with error
+  // }
+  user_location.coords[0] = user_lat;
+  user_location.coords[1] = user_lon;
   strcpy(user_location.name, "User");
 
   KDNode *best = NULL;
   double best_dist = DBL_MAX;
   nearest_neighbor(root, &user_location, &best, &best_dist);
 
-  printf("\nYour Location:\n");
-  print_point(&user_location);
-  printf("\nNearest Location:\n");
-  print_point(&best->point);
-  printf("Distance: %.6f\n", best_dist);
+  // printf("\nYour Location:\n");
+  // print_point(&user_location);
+  // printf("\nNearest Location:\n");
+  // print_point(&best->point);
+  // printf("Distance: %.6f\n", best_dist);
+
+  printf("{\"name\": \"%s\", \"lat\": %lf, \"lon\": %lf}\n",
+           best->point.name, best->point.coords[0], best->point.coords[1]);
 
   free_kd_tree(root);
   return 0;
