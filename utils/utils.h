7fa4fc2486496e4b6b483077c9e5630b675b83f5
balbir <prasadbalbir1056@gmail.com>
2025-04-16 09:03:52 +0530

    rename path according to new files

--- utils/utils.h
+++ utils/utils.h
@@ -1,6 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
-#include "../src/point.h"
+#include "../src/kd_tree.h"
 #include <stdio.h>
 #include <stdlib.h>
 int load_points(const char *filename, Point points[], int max_points);
